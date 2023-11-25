b8f0a5530267b1b54b2a70fc223a34e2047a652b
Muhammad Umair Shahid <103985768+umairshahid-1@users.noreply.github.com>
2023-11-25 15:39:19 +0500

    Fix and add signup api
    This commit adds files for web implementation and config.dart which is global values for apis setting. Also signup is working corrrectly.

--- DiscordCloneFrontend/lib/apiCalls/config.dart
+++ DiscordCloneFrontend/lib/apiCalls/config.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://localhost:3076/api';
+  static const String baseUrl = 'http://10.5.22.37:3076/api';
   static const String loginUserApi = '$baseUrl/users/login';
   static const String signupApi = '$baseUrl/signup';
 }
