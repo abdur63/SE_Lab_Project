19f6aeb043097d75ea909ccf37f50e2462dd58f5
Muhammad Umair Shahid <103985768+umairshahid-1@users.noreply.github.com>
2023-11-27 22:21:37 +0500

    Update the BaseURL in config.dart
    This commits is very important because in order to test this on mobile we need to update every time we run this project from start. And we do it by running "ipconfig" in cmd prompt or terminal by copying IPv4 address.

--- DiscordCloneFrontend/lib/apiCalls/config.dart
+++ DiscordCloneFrontend/lib/apiCalls/config.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = 'http://10.10.31.151:3076/api';
+  static const String baseUrl = 'http://10.5.23.80:3076/api';
   static const String signupApi = '$baseUrl/signup';
   static const String loginUserApi = '$baseUrl/user/login';
 }
