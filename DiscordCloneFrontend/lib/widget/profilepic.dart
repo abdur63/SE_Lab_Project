544ec9768858d19f469ddb8b8045a8c6e9afca3b
Muhammad Umair Shahid <103985768+umairshahid-1@users.noreply.github.com>
2023-11-28 00:09:41 +0500

    Add default profile picture

--- DiscordCloneFrontend/lib/widget/profilepic.dart
+++ DiscordCloneFrontend/lib/widget/profilepic.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:discord/model/user.model.dart';
 import 'package:flutter/material.dart';
 
@@ -12,19 +11,23 @@ class ProfilePic extends StatelessWidget {
       alignment: Alignment.bottomRight,
       children: <Widget>[
         ClipRRect(
-            borderRadius: BorderRadius.circular(100),
-            child: Container(
-                width: 90,
+          borderRadius: BorderRadius.circular(100),
+          child: Container(
+            width: 90,
+            height: 90,
+            color: const Color.fromARGB(255, 26, 24, 24),
+            padding: const EdgeInsets.all(7),
+            alignment: Alignment.center,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: const Image(
+                image: AssetImage('assets/discord_profile_pic.png'),
                 height: 90,
-                color: const Color.fromARGB(255, 26, 24, 24),
-                padding: const EdgeInsets.all(7),
-                alignment: Alignment.center,
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: CachedNetworkImage(
-                        height: 90,
-                        width: 90,
-                        imageUrl: userData.profilePic)))),
+                width: 90,
+              ),
+            ),
+          ),
+        ),
         Padding(
           padding: const EdgeInsets.all(9),
           child: userData.status == 'Inactive'
@@ -44,21 +47,22 @@ class ProfilePic extends StatelessWidget {
                         height: 3,
                         width: 12,
                         color: Colors.black,
-                      ))
+                      ),
+                    )
                   : userData.status != 'Online'
                       ? Container(
                           decoration: const BoxDecoration(
-                              color: Colors.grey, shape: BoxShape.circle),
+                              color: Colors.green, shape: BoxShape.circle),
                           height: 18,
                           width: 18,
                         )
                       : Container(
                           decoration: const BoxDecoration(
-                              color: Colors.green, shape: BoxShape.circle),
+                              color: Colors.grey, shape: BoxShape.circle),
                           height: 18,
                           width: 18,
                         ),
-        )
+        ),
       ],
     );
   }
